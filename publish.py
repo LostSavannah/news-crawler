71bc279de257a22bb6e37acbe8510dce8cd899c2
codeRookieErick <erickfernandomoraramirez@gmail.com>
2022-11-13 08:33:37 -0400

    Testing the action runner pipeline

--- publish.py
+++ publish.py
@@ -9,9 +9,10 @@ fileName = os.sep.join([baseFolder, "log.txt"])
 currentFolder:str = os.path.abspath(os.curdir)
 
 def copio(src:str, dest:str, *, follow_symlinks=True):
-    pass
+    print(src, dest)
+    return dest
 
 def ignore(str:str, dirs:List[str]) -> List[str]:
     return [i for i in dirs if not i.startswith('.')]
 
-shutil.copytree(currentFolder, baseFolder, dirs_exist_ok=True, ignore=ignore)
+shutil.copytree(currentFolder, baseFolder, copy_function=copio, dirs_exist_ok=True, ignore=ignore)
