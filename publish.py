ef9b8239500a0a9ed27a70e71e67901f5bc0fed8
codeRookieErick <erickfernandomoraramirez@gmail.com>
2022-11-13 09:16:45 -0400

    Testing the action runner pipeline

--- publish.py
+++ publish.py
@@ -1,18 +1,38 @@
 import shutil
 import os
+import sys
 from typing import List
 
 path:List[str] = ["", "scripts", "python", "news_crawler"]
+#path:List[str] = ["D:", "test", "copytree"]
 baseFolder = os.sep.join(path)
-fileName = os.sep.join([baseFolder, "log.txt"])
 
 currentFolder:str = os.path.abspath(os.curdir)
 
-def copio(src:str, dest:str, *, follow_symlinks=True):
-    print(src, dest)
-    return dest
+def get_files(path:str, folder:str='.', ignoreFolders:List[str] = None):
+    ignoreFolders = ignoreFolders or []
+    for f in os.listdir(path):
+        if f in ['.', '..']: continue
+        dir:str = os.sep.join([path, f])
+        if os.path.isfile(dir):
+            yield (path, folder, f)
+        elif f in ignoreFolders:
+            continue
+        else:
+            for r in get_files(dir, os.sep.join([folder, f]), ignoreFolders):
+                yield r
 
-def ignore(str:str, dirs:List[str]) -> List[str]:
-    return [i for i in dirs if not i.startswith('.')]
+def copytree(src:str, dest:str):
+    for i in get_files(src, ignoreFolders=['.git', '__pycache__']):
+        baseFolder, folder, file = i
+        folderPaths = folder.split(os.sep)
+        folderPaths[0] = dest
+        destinationFolder = os.sep.join(folderPaths)
+        os.makedirs(destinationFolder, exist_ok=True)
+        sourceFile = os.sep.join([baseFolder, file])
+        destinationFile = os.sep.join([destinationFolder, file])
+        if os.path.isfile(destinationFile):
+            os.unlink(destinationFile)
+        shutil.copy(sourceFile, destinationFile)
 
-shutil.copytree(currentFolder, baseFolder, copy_function=copio, dirs_exist_ok=True, ignore=ignore)
+copytree(currentFolder, baseFolder)
