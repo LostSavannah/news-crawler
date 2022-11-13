3304c2c050dcf8c9dbcc5f117cfd0935054a242f
codeRookieErick <erickfernandomoraramirez@gmail.com>
2022-11-13 08:04:33 -0400

    Testing the actions runner

--- publish.py
+++ publish.py
@@ -1,3 +1,4 @@
+import shutil
 import os
 from typing import List
 
@@ -5,5 +6,13 @@ path:List[str] = ["", "scripts", "python", "news_crawler"]
 baseFolder = os.sep.join(path)
 fileName = os.sep.join([baseFolder, "log.txt"])
 
+foldersToCopy:List[str] = ["config", "database", "miner"]
+
 with open(fileName, "a", encoding='latin-1') as file:
     file.write("Hello moto")
+
+
+def copio(src:str, dest:str, *, follow_symlinks=True):
+    print(src, dest)
+
+shutil.copytree('.', baseFolder, copy_function=copio)
